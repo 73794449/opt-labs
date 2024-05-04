88d50fb85b39d95315b0070fee571d4c93033566
t3ry444y <t3ry444y@gmail.com>
2024-05-04 13:08:08 +0300

    i like macros

--- src/util/verify.c
+++ src/util/verify.c
@@ -9,58 +9,62 @@ size_t o_row = 0, o_col = 0;
 size_t v_row = 0, v_col = 0;
 
 #define step_macro(x, row, col)                                                \
-  if (x == '\n') {                                                             \
-    row++;                                                                     \
-    col = 0;                                                                   \
-  } else if (x == '\t') {                                                      \
-    col += 4;                                                                  \
-  } else {                                                                     \
-    col++;                                                                     \
-  }
+  do {                                                                         \
+    if (x == '\n') {                                                           \
+      row++;                                                                   \
+      col = 0;                                                                 \
+    } else if (x == '\t') {                                                    \
+      col += 4;                                                                \
+    } else {                                                                   \
+      col++;                                                                   \
+    }                                                                          \
+  } while (0)
 
-void step(bool is_c) {
-  if (is_c) {
-    step_macro(c, o_row, o_col);
-  } else {
-    step_macro(d, v_row, v_col);
+#define step_c() step_macro(c, o_row, o_col)
+#define step_d() step_macro(d, v_row, v_col)
+#define if_step(x)                                                             \
+  do {                                                                         \
+    if (x)                                                                     \
+      step_c();                                                                \
+    else                                                                       \
+      step_d();                                                                \
+  } while (0)
+#define skip_ws_char(x, flag)                                                  \
+  do {                                                                         \
+    if (symbol_type(x) == SYMBOL_WS) {                                         \
+      x = (char)getc(_out);                                                    \
+      if_step(flag);                                                           \
+      skip_ws(_out, _ver, flag);                                               \
+    }                                                                          \
+  } while (0)
+#define open_read_file(pname, filename)                                        \
+  FILE *pname = fopen(filename, "r");                                          \
+  if (_out == NULL) {                                                          \
+    printf("Failed to open output file on verify stage\n");                    \
+    exit(EXIT_FAILURE);                                                        \
   }
-}
 
 void skip_ws(FILE *_out, FILE *_ver, bool is_c) {
-  if (is_c) {
-    if (symbol_type(c) == SYMBOL_WS) {
-      c = (char)getc(_out);
-      skip_ws(_out, _ver, true);
-    }
-  } else {
-    if (symbol_type(d) == SYMBOL_WS) {
-      d = (char)getc(_ver);
-      skip_ws(_out, _ver, false);
-    }
-  }
+  if (is_c)
+    skip_ws_char(c, true);
+  else
+    skip_ws_char(d, false);
 }
 
 void verify(char *_output, char *_verify) {
-  FILE *_out = fopen(_output, "r");
-  if (_out == NULL) {
-    printf("Failed to open output file on verify stage\n");
-    exit(EXIT_FAILURE);
-  }
-  FILE *_ver = fopen(_verify, "r");
-  if (_ver == NULL) {
-    printf("Failed to open verify file on verify stage\n");
-    exit(EXIT_FAILURE);
-  }
+  open_read_file(_out, _output);
+  open_read_file(_ver, _verify);
 
   do {
     c = (char)getc(_out);
     d = (char)getc(_ver);
+    step_c();
+    step_d();
     skip_ws(_out, _ver, true);
     skip_ws(_out, _ver, false);
-    if (c != d) {
+    if (c != d)
       printf("Output(%llu:%llu:%c) != Verify(%llu:%llu:%c)\n", o_row + 1, o_col,
              c, v_row + 1, v_col, d);
-    }
   } while (!(c == EOF || d == EOF));
 
   fclose(_out);
